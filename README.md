672bd6391ebb1f54380bd78eb95b3eb08f80ecbd
Petri Koivula <pkjedi@iki.fi>
2014-03-17 20:23:51 +0200

    Configurable, push to Geckboard

--- README.md
+++ README.md
@@ -1,6 +1,25 @@
 # Travis to Geckoboard daemon
 
-A simplistic daemon that fetches travis-ci job information and pushes it into a geckoboard widget.
+A daemon that fetches [Travis CI](https://travis-ci.org/) job information
+and pushes it into a [Geckoboard](http://www.geckoboard.com/) custom text widget.
+
+## Features
+
+- Works for open and private repositories
+- Configurable Travis CI fetch interval
+- Only pushes to Geckoboard after a new build
+
+## Limitations
+
+- Currently only watches finished builds spawned by pushes to the _master_ branch
+- Geckoboard push data is not configurable or templated
+- No tests, will die in case of network issues
+- Likely will not work with a large amount of repositories
+- "my first" level [Reactive Extensions](https://github.com/Reactive-Extensions/RxJS) based code
+
+## Configure
+
+Copy _config.example.json_ to _config.json_ and edit to your needs.
 
 ## Run
 
--- daemon.js
+++ daemon.js
@@ -6,29 +6,16 @@ const Rx = require('rx');
 const https = require('https');
 const _ = require('underscore');
 
-// TODO repo config sanity
+const config = require('./config.json');
 
-var repos = {
-    7900: {
-        name: 'xi-project/xi-filelib',
-        status: {number: "0"}
-    },
-    30296: {
-        name: 'xi-project/xi-bundle-filelib',
-        status: {number: "0"}
-    }
-};
-
-// TODO travis token(s)
-
-var repoUrl = function(repo) {
-    return 'https://api.travis-ci.org/repos/' + repo + '/builds';
-};
+var repos = _(_.clone(config.repos)).each(function(repo) {
+    _.extend(repo, {status: {number: '0'}});
+});
 
 Rx.Observable
-    .interval(60 * 1000)
+    .interval(config.interval * 1000)
     .flatMap(function() {
-        return Rx.Observable.fromArray(_(repos).chain().values().pluck('name').map(repoUrl).value());
+        return Rx.Observable.fromArray(_(repos).chain().values().pluck('url').value());
     })
     .flatMap(function(url) {
         return Rx.Observable.fromCallback(https.get)(url);
@@ -55,8 +42,52 @@ Rx.Observable
 
         return pass;
     })
-    .throttle(5 * 1000)
-    .subscribe(function(build) {
-        console.log(repos);
-        // TODO push to geckoboard
+    .throttle(10 * 1000)
+    .map(function() {
+        return _(repos).chain().values().map(function(repo) {
+            var status = repo.status.result !== 0 ? 'negative' : 'positive';
+            return '<p class="' + status + ' t-size-x18">' +
+                repo.name +
+                ': ' + repo.status.number +
+                ', ' + repo.status.branch +
+            '</p>';
+        }).join('\n').value();
+    })
+    .flatMap(function(statusHtml) {
+        return Rx.Observable
+            .fromCallback(function(callback) {
+                var data = JSON.stringify({
+                    'api_key': config.geckoboard.api_key,
+                    'data': {
+                        'item': [
+                            {
+                                'text': statusHtml,
+                                'type': 0
+                            }
+                        ]
+                    }
+                });
+                var request = https.request({
+                    host: 'push.geckoboard.com',
+                    port: '443',
+                    path: '/v1/send/' + config.geckoboard.widget_key,
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Content-Length': data.length
+                    }
+                }, callback);
+                request.write(data);
+                request.end();
+            })();
+    })
+    .flatMap(function(response) {
+        response.setEncoding('utf8');
+        return Rx.Observable.fromEvent(response, 'data')
+            .buffer(Rx.Observable.fromEvent(response, 'end'))
+            .map(function(parts) { return parts.join(''); })
+            .map(JSON.parse);
+    })
+    .subscribe(function(response) {
+        console.log(response);
     });
