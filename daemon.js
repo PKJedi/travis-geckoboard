7975eb1061a1121ed464e52651cb88721af8b380
Petri Koivula <pkjedi@iki.fi>
2014-03-17 20:34:58 +0200

    Small refactors

--- daemon.js
+++ daemon.js
@@ -12,6 +12,14 @@ var repos = _(_.clone(config.repos)).each(function(repo) {
     _.extend(repo, {status: {number: '0'}});
 });
 
+var observableFromJsonResponse = function(response) {
+    response.setEncoding('utf8');
+    return Rx.Observable.fromEvent(response, 'data')
+        .buffer(Rx.Observable.fromEvent(response, 'end'))
+        .map(function(parts) { return parts.join(''); })
+        .map(JSON.parse);
+};
+
 Rx.Observable
     .interval(config.interval * 1000)
     .flatMap(function() {
@@ -20,13 +28,7 @@ Rx.Observable
     .flatMap(function(url) {
         return Rx.Observable.fromCallback(https.get)(url);
     })
-    .flatMap(function(response) {
-        response.setEncoding('utf8');
-        return Rx.Observable.fromEvent(response, 'data')
-            .buffer(Rx.Observable.fromEvent(response, 'end'))
-            .map(function(parts) { return parts.join(''); })
-            .map(JSON.parse);
-    })
+    .flatMap(observableFromJsonResponse)
     .flatMap(function(builds) {
         return Rx.Observable.fromArray(builds);
     })
@@ -46,6 +48,7 @@ Rx.Observable
     .map(function() {
         return _(repos).chain().values().map(function(repo) {
             var status = repo.status.result !== 0 ? 'negative' : 'positive';
+
             return '<p class="' + status + ' t-size-x18">' +
                 repo.name +
                 ': ' + repo.status.number +
@@ -54,40 +57,27 @@ Rx.Observable
         }).join('\n').value();
     })
     .flatMap(function(statusHtml) {
-        return Rx.Observable
-            .fromCallback(function(callback) {
-                var data = JSON.stringify({
-                    'api_key': config.geckoboard.api_key,
-                    'data': {
-                        'item': [
-                            {
-                                'text': statusHtml,
-                                'type': 0
-                            }
-                        ]
-                    }
-                });
-                var request = https.request({
-                    host: 'push.geckoboard.com',
-                    port: '443',
-                    path: '/v1/send/' + config.geckoboard.widget_key,
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Content-Length': data.length
-                    }
-                }, callback);
-                request.write(data);
-                request.end();
-            })();
+        return Rx.Observable.fromCallback(function(callback) {
+            var data = JSON.stringify({
+                api_key: config.geckoboard.api_key,
+                data: {item: [{text: statusHtml, type: 0}]}
+            });
+            var options = {
+                host: 'push.geckoboard.com',
+                port: '443',
+                path: '/v1/send/' + config.geckoboard.widget_key,
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Content-Length': data.length
+                }
+            };
+            var request = https.request(options, callback);
+            request.write(data);
+            request.end();
+        })();
     })
-    .flatMap(function(response) {
-        response.setEncoding('utf8');
-        return Rx.Observable.fromEvent(response, 'data')
-            .buffer(Rx.Observable.fromEvent(response, 'end'))
-            .map(function(parts) { return parts.join(''); })
-            .map(JSON.parse);
-    })
+    .flatMap(observableFromJsonResponse)
     .subscribe(function(response) {
         console.log(response);
     });
