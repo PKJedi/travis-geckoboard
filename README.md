73473cec1c7580ae07a64e7aef2ff70236cfc2e0
Petri Koivula <pkjedi@iki.fi>
2014-03-18 20:01:24 +0200

    Refactored to better functional style

--- README.md
+++ README.md
@@ -7,15 +7,12 @@ and pushes it into a [Geckoboard](http://www.geckoboard.com/) custom text widget
 
 - Works for open and private repositories
 - Configurable Travis CI fetch interval
-- Only pushes to Geckoboard after a new build
 
 ## Limitations
 
 - Currently only watches finished builds spawned by pushes to the _master_ branch
 - Geckoboard push data is not configurable or templated
-- No tests, will die in case of network issues
-- Likely will not work with a large amount of repositories
-- "my first" level [Reactive Extensions](https://github.com/Reactive-Extensions/RxJS) based code
+- No tests
 
 ## Configure
 
--- daemon.js
+++ daemon.js
@@ -3,87 +3,39 @@
 'use strict';
 
 const Rx = require('rx');
-const https = require('https');
-const _ = require('underscore');
-
 const config = require('./config.json');
-
-var repos = _(_.clone(config.repos)).each(function(repo) {
-    _.extend(repo, {status: {number: '0'}});
-});
-
-var observableFromJsonResponse = function(response) {
-    response.setEncoding('utf8');
-    return Rx.Observable.fromEvent(response, 'data')
-        .buffer(Rx.Observable.fromEvent(response, 'end'))
-        .map(function(parts) { return parts.join(''); })
-        .map(JSON.parse);
-};
-
-/*
- * TODO refactor such that the outer Observable is always on the "all" level
- * Currently all -> repositories -> builds -> all, with nasty mutable state
- */
+const travis = require('./lib/travis.js');
+const geckoboard = require('./lib/geckoboard.js');
+const repositoriesToHtml = require('./lib/repositoriesToHtml.js');
 
 Rx.Observable
+
+    // Every config.interval seconds,
     .interval(config.interval * 1000)
+
+    // fetch build information from Travis CI.
     .flatMap(function() {
-        return Rx.Observable.fromArray(_(repos).chain().values().pluck('url').value());
+        return travis(config.repos);
     })
-    .flatMap(function(url) {
-        return Rx.Observable.fromCallback(https.get)(url);
-    })
-    .flatMap(observableFromJsonResponse)
-    .flatMap(function(builds) {
-        return Rx.Observable.fromArray(builds);
-    })
-    .filter(function(build) {
-        return build.branch === 'master' && build.event_type === 'push' && build.state === 'finished';
-    })
-    .filter(function(build) {
-        var pass = parseInt(build.number, 10) > parseInt(repos[build.repository_id].status.number, 10);
 
-        if (pass) {
-            repos[build.repository_id].status = _.clone(build);
+    // Format the build information and
+    .map(repositoriesToHtml)
+
+    // push it onto Geckoboard.
+    .flatMap(function(html) {
+        return geckoboard.customText(
+            config.geckoboard.api_key,
+            config.geckoboard.widget_key,
+            [{text: html, type: 0}]
+        );
+    })
+
+    // Output Geckoboard response or possible errors.
+    .subscribe(
+        function(response) {
+            console.log(response);
+        },
+        function(error) {
+            console.log(error);
         }
-
-        return pass;
-    })
-    .throttle(10 * 1000)
-    .map(function() {
-        return _(repos).chain().values().map(function(repo) {
-            var status = repo.status.result !== 0 ? 'negative' : 'positive';
-
-            return '<p class="' + status + ' t-size-x18">' +
-                repo.name +
-                ': ' + repo.status.number +
-                ', ' + repo.status.branch +
-            '</p>';
-        }).join('\n').value();
-    })
-    .flatMap(function(statusHtml) {
-        return Rx.Observable.fromCallback(function(callback) {
-            var data = JSON.stringify({
-                api_key: config.geckoboard.api_key,
-                data: {item: [{text: statusHtml, type: 0}]}
-            });
-            var options = {
-                host: 'push.geckoboard.com',
-                port: '443',
-                path: '/v1/send/' + config.geckoboard.widget_key,
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Content-Length': data.length
-                }
-            };
-            var request = https.request(options, callback);
-            request.write(data);
-            request.end();
-        })();
-    })
-    .flatMap(observableFromJsonResponse)
-    .subscribe(function(response) {
-        console.log(response);
-    });
-
+    );
